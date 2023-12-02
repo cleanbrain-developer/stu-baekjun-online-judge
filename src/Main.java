d1bb7d60234a6202cd579a392e7e1195e3519ec6
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-02 21:22:44 +0900

    feat. 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 /**
  * <pre>
  * User: clean_brain
@@ -7,6 +10,9 @@
  */
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		BigInteger a = scanner.nextBigInteger();
+		BigInteger b = scanner.nextBigInteger();
+		System.out.println(a.add(b.multiply(new BigInteger("-1"))).abs());
 	}
 }

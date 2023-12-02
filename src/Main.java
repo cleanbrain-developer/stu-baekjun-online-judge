0ba782d2ad3700959f42c0f831c67e45e21b07c7
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-02 20:24:04 +0900

    feat. 풀이 완료

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+/**
+ * <pre>
+ * User: clean_brain
+ * Date: 2023-12-02
+ * Comments: 
+ * </pre>
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(a + b);
+	}
+}

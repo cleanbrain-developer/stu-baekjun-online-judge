10d1416b1d938be5b289a083baa9d8186c5478b6
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-02 22:12:34 +0900

    feat. 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * <pre>
  * User: clean_brain
@@ -7,6 +11,22 @@
  */
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.next();
+		Map<String, String> map = new HashMap<>();
+		map.put("A+", "4.3");
+		map.put("A0", "4.0");
+		map.put("A-", "3.7");
+		map.put("B+", "3.3");
+		map.put("B0", "3.0");
+		map.put("B-", "2.7");
+		map.put("C+", "2.3");
+		map.put("C0", "2.0");
+		map.put("C-", "1.7");
+		map.put("D+", "1.3");
+		map.put("D0", "1.0");
+		map.put("D-", "0.7");
+		map.put("F", "0.0");
+		System.out.println(map.get(s));
 	}
 }

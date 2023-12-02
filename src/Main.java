92efb8c3304493dc2d672863ff69e3de6bb195f2
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-02 21:34:34 +0900

    feat. 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * <pre>
  * User: clean_brain
@@ -7,6 +9,9 @@
  */
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(a + b);
 	}
 }

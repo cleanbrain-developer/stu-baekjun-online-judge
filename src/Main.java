2ad4e18fd3342ca84e0149a6d310978299dd775b
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-02 22:02:10 +0900

    feat. 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * <pre>
  * User: clean_brain
@@ -7,6 +9,18 @@
  */
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.next();
+		char[] charArray = s.toCharArray();
+		char[] resAry = new char[s.length()];
+		for (int i = 0; i < charArray.length; i++) {
+			char c = charArray[i];
+			if (Character.isLowerCase(c)) {
+				resAry[i] = Character.toUpperCase(c);
+			} else if(Character.isUpperCase(c)) {
+				resAry[i] = Character.toLowerCase(c);
+			}
+		}
+		System.out.println(resAry);
 	}
 }

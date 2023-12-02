3e02594ee13384a9d859540ae09f876631a92926
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-02 22:07:06 +0900

    feat. 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * <pre>
  * User: clean_brain
@@ -7,6 +9,12 @@
  */
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		int year = scanner.nextInt();
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
 	}
 }

64069c912784286d89cf11f46f44c096c9bfc925
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-02 21:31:28 +0900

    feat. 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 /**
  * <pre>
  * User: clean_brain
@@ -7,6 +10,12 @@
  */
 public class Main {
 	public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+		BigInteger a = scanner.nextBigInteger();
+		BigInteger b = scanner.nextBigInteger();
+		BigInteger c = scanner.nextBigInteger();
+		BigInteger d = scanner.nextBigInteger();
+		BigInteger e = scanner.nextBigInteger();
+		System.out.println((a.pow(2).add(b.pow(2)).add(c.pow(2)).add(d.pow(2)).add(e.pow(2))).mod(new BigInteger("10")));
 	}
 }

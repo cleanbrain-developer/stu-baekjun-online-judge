88a216f415f0ad4b7812cd0b9454edf1ff1398f7
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-02 21:13:14 +0900

    feat. 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@
  */
 public class Main {
 	public static void main(String[] args) {
-
+		System.out.println("(___)");
+		System.out.println("(o o)____/");
+		System.out.println(" @@      \\");
+		System.out.println("  \\ ____,/");
+		System.out.println("  //   //");
+		System.out.println(" ^^   ^^");
 	}
 }

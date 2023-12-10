45d42741eb78e82ad3a9f10aab6b9c01ed8663e6
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-10 18:08:01 +0900

    feat. 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 /**
  * <pre>
  * User: clean_brain
  * Date: 2023-12-02
- * Comments: 
+ * Comments:
  * </pre>
  */
 public class Main {
-	public static void main(String[] args) {
-
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String jaehwan = scanner.next();
+        String doctor = scanner.next();
+        long jaehwanCount = jaehwan.chars()
+                .mapToObj(value -> (char) value)
+                .collect(Collectors.toList())
+                .stream()
+                .filter(character -> character == 'a')
+                .count();
+        long doctorCount = doctor.chars()
+                .mapToObj(value -> (char) value)
+                .collect(Collectors.toList())
+                .stream()
+                .filter(character -> character == 'a')
+                .count();
+        if (jaehwanCount >= doctorCount) {
+            System.out.println("go");
+        } else {
+            System.out.println("no");
+        }
+    }
 }

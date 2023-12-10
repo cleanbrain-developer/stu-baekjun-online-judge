1fc2e262bf143618a945d1a0633b18d890e3bdfa
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-12-10 18:11:29 +0900

    feat. 풀이 완료

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,20 @@
  * <pre>
  * User: clean_brain
  * Date: 2023-12-02
- * Comments: 
+ * Comments:
  * </pre>
  */
 public class Main {
-	public static void main(String[] args) {
-
-	}
+    public static void main(String[] args) {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.print("|          |");
+    }
 }
